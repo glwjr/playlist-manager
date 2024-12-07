e4dc2fe66800a04445a7683c13726d047ceea67c
Gary White <glw494@gmail.com>
2024-12-06 20:10:50 -0800

    FallbackController update

--- server/src/main/java/edu/sdccd/cisc191/server/util/FallbackController.java
+++ server/src/main/java/edu/sdccd/cisc191/server/util/FallbackController.java
@@ -1,22 +1,18 @@
 package edu.sdccd.cisc191.server.util;
 
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FallbackController {
-
-    // Defaults to a blank white page on unmapped routes
-    @GetMapping("/**")
+    
+    @RequestMapping("/**")
     public ResponseEntity<String> handleFallback() {
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Content-Type", "text/html; charset=UTF-8");
-
+        String blankPage = "<html><head></head><body></body></html>";
         return ResponseEntity.ok()
-                .headers(headers)
-                .body("<html><head></head><body></body></html>");
+                .header("Content-Type", "text/html; charset=UTF-8")
+                .body(blankPage);
     }
 
 }
